1bcb1946656308d6a1a02faa1df27b9b94edaa37
sergiorodas <sergiorodasgonzalez@gmail.com>
2022-06-06 05:20:06 -0300

    Agrega funcionalidad de inicio de sesion y descarga de archivo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,23 @@
+require('dotenv').config();
 const { Router } = require('express');
 const router = Router();
 const puppeteer = require('puppeteer');
+const path = require('path');
 
 router.get('/', (req, res) => {
     res.json({message: 'Hello World!!'})
 });
 
-const options = { 
-    executablePath: '/usr/bin/google-chrome',
-    headless: true, // true
+// Configuración del navegador y opciones de Hatch
+const urlToParse = "https://my.gethatch.com";
+const hatchEmail = process.env.HATCH_EMAIL;
+const hatchPassword = process.env.HATCH_PASSWORD;
+
+const downloadPath = path.resolve(__dirname, '../../data');
+const browserOptions = { 
+    executablePath: '/usr/bin/google-chrome', // comentar esta línea en local
+    headless: true, // true en docker / false en local
+    defaultViewport: null,
     args: [
         "--disable-gpu",
         "--disable-dev-shm-usage",
@@ -16,26 +25,55 @@ const options = {
         "--no-sandbox",
     ] 
 };
-
-const urlToParse = "https://my.gethatch.com";
+const pageOptions = {
+    waitUntil: 'networkidle2',
+    // Remove the timeout
+    timeout: 0
+}
 
 router.get('/all', (req, res) => {
-    puppeteer.launch(options).then((browser) => {
-        browser.newPage().then((page) => {
-            page.goto(urlToParse, {
-                waitUntil: 'networkidle2',
-                // Remove the timeout
-                timeout: 0
-            }).then(() => {
-                page.screenshot({path:'image.png'});
-                console.log('Se tomó la captura');
-            })
-        })
-    }).catch((e) => {
-        console.log('Falló: ', e);
-    });
+    console.log("Comienza la búsqueda de la data");
+    (async () => {
+        const browser = await puppeteer.launch(browserOptions);
+        const page = await browser.newPage();
+        await page.goto(urlToParse, pageOptions);
+        await page.waitForSelector(".MuiButton-root");
+        await page.type("#mui-1", hatchEmail);
+        await page.type("#mui-2", hatchPassword);
+        await page.click(".MuiButton-root");
+        // Finaliza logueo
+        await page.waitForTimeout(10000);
+        // Selecciona el retailer
+        await page.waitForSelector("#mui-12");
+        await page.click("#mui-12");
+        await page.type("#mui-12", "currys");
+        await page.focus("#mui-12");
+        await page.keyboard.press('ArrowDown');
+        await page.keyboard.press('Enter');
+        // Establezco la ubicación de la carpeta donde se va a descargar el archivo
+        await page._client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: downloadPath 
+        });
+        // Click en el boton de descargar
+        await page.evaluate(() => {
+            let saveButton = document.querySelectorAll('.MuiIconButton-sizeLarge'); 
+            saveButton[0].click();
+        });
+        await page.waitForTimeout(1000);
+        // Selecciona la opción "Lead Data"
+        await page.evaluate(() => {
+            let saveButton = document.querySelectorAll('.MuiButton-outlinedInherit');
+            saveButton[0].click();
+        });
+        await page.waitForTimeout(10000);
+        // await browser.close();
+        console.log('Se completó la descarga');
+    })()
+    .catch(err => console.error(err));
 
     res.json({message: 'Hello All!!'})
 });
+
 
 module.exports = router;
